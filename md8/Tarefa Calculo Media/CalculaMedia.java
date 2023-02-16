75ba3d06f346d5eb7db46a186410b1fa213f8a53
Márcio Fontes <contato@marciofontes.com>
2023-02-16 09:45:33 -0300

    Enviando Tarefa de Cálculo da Média
    Enviando o exercício para a correção!

---
+++ md8/Tarefa Calculo Media/CalculaMedia.java
@@ -0,0 +1,28 @@
+package br.com.marciofontes;
+
+import java.util.Scanner;
+
+public class CalculaMedia {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("--- Vamos calcular a média de 4 notas, tudo bem? ---");
+		System.out.println("--- Digite a nota numero 1:");
+		double nota1 = entrada.nextDouble();
+
+		System.out.println("--- Digite a nota numero 2:");
+		double nota2 = entrada.nextDouble();
+
+		System.out.println("--- Digite a nota numero 3:");
+		double nota3 = entrada.nextDouble();
+
+		System.out.println("--- Digite a nota numero 4:");
+		double nota4 = entrada.nextDouble();
+
+		double media = (nota1 + nota2 + nota3 + nota4) / 4;
+		System.out.printf("\n A média das suas notas é: %2.2f " , media);
+	}
+
+}
